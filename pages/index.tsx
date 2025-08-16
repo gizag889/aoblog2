a0607906a5767e4001999970ffe47f5788ab2d04
gizag889 <gizaguri0426@outlook.jp>
2025-08-16 20:41:06 +0900

    [add]useSWR実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import type { NextPage } from 'next'
 import PostService from '../services/PostService'
 import PostType from '@/types/PostType'
+import usePostListSwr from '@/hooks/swr/usePostListSwr'
 
 
 const Home: NextPage<{
   staticPostList: PostType[] // 型の指定をする場所に注意！
 }> = ({ staticPostList }) => {
-
+  const postList = usePostListSwr(staticPostList)
+  //useSWRは二重チェックの役割
   return (
     <div>
-      {staticPostList.map((post) => {
+      {postList?.map((post) => {
+        //このmapでusePostListSwrで渡した書き換えたGraphQlツリーが反映される
+        
         return <p key={post.id}>{post.title}</p> // 一個ずつ表示させる
       })}
     </div>
