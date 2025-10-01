4e217f3ddb84fbc16f174af6d42b16aa96c06a27
gizag889 <gizaguri0426@outlook.jp>
2025-10-01 15:26:20 +0900

    [add]refs#2カードの投稿日時調整ロジック追加

--- components/molecules/PostBox.tsx
+++ components/molecules/PostBox.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 // type
 import PostType from '@/types/PostType'
+//component
+import DateText from '../atoms/Text/DateText'
 
 
 
@@ -11,17 +13,16 @@ const PostBox = ({ post }: {
     post: PostType
 }) => {
   return ( 
-    <div className='w-1/2'>       
-        <div key={post.id} className='border-1 border-(--color-divider-main) rounded-lg'>
-            <div >
+    <div className='w-1/2 cursor-pointer '>       
+        <div key={post.id} className=' border-1 border-(--color-divider-main) hover:border-(--color-secondary-main) rounded-lg'>
+            <div className='hover:text-(--color-secondary-main)'>
                     <img 
                         className='w-full h-56 object-cover rounded-t-lg'
                         src={post.featuredImage.url} /> {/* 詳細編で<Image />に変える */}
-                    <div className='p-4'>
-                        <span>{post.category.name}</span>
-                        <h1 className='font-bold'>{post.title}</h1>
-                        <div dangerouslySetInnerHTML={{__html: post.excerpt}}></div> 
-                        <span>{post.date}</span>
+                    <div className='p-4 '>
+                        <span >{post.category.name}</span>
+                        <h1 className='font-bold pt-2'>{post.title}</h1>
+                        <DateText>{post.date}</DateText>
                     </div>
                     
             </div>
---
+++ pages/post/[slug].tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+const Post: NextPage = () => {
+  return (
+    <div>記事詳細です</div>
+  )
+}
+
+export default Post
